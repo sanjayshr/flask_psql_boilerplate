080145523ed700bdddc0d2d8ab8f8c96c9cd3983
berlin0101 <sanjaysbgl555@gmail.com>
2023-06-18 09:08:47 +0530

    added hello module, ADD requirements.txt

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,26 +1,15 @@
 from flask import Blueprint, jsonify, request
-from app.models.user import User
-from app.database import db
+from app.services.user_service import get_all_users, create_new_user
 
 user = Blueprint('user', __name__)
 
-@user.route('/api/test', methods=['GET'])
-def test():
-    return jsonify({'message': 'Hello, World!'})
-
 @user.route('/api/users', methods=['GET'])
 def get_users():
-    users = User.query.all()
-    return jsonify([user.to_dict() for user in users])
+    return jsonify(get_all_users())
 
 @user.route('/api/users', methods=['POST'])
 def create_user():
     data = request.get_json() or {}
-    user = User()
-    user.username = data.get('username')
-    user.email = data.get('email')
-    user.set_password(data.get('password'))
-    user.from_dict(data)
-    db.session.add(user)
-    db.session.commit()
-    return jsonify(user.to_dict()), 201
+    new_user =  create_new_user(data)
+    return jsonify(new_user), 201
+    
